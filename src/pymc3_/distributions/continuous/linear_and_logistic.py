dc8bcf4992b32aec18b51375a8b964747fb19dba
Ryan Nazareth <ryankarlos@gmail.com>
2021-04-18 19:40:39 +0100

    add examples of pymc3 linear and logistic and Bayesian networks notebook

--- src/pymc3_/distributions/continuous/linear_and_logistic.py
+++ src/pymc3_/distributions/continuous/linear_and_logistic.py
@@ -1,14 +1,17 @@
 import pymc3 as pm
 from data.data import simulate_data_lr
 from typing import Callable
-from pymc3_.map_and_sampling import map_estimation
-from pymc3 import Normal, HalfCauchy, plot_posterior_predictive_glm
-import matplotlib.pyplot as plt
+from src.pymc3_.plot import traceplot, plot_posterior_predictive_samples
+from src.pymc3_.map_and_sampling import map_estimation
+from pymc3 import Normal, HalfCauchy
 from functools import partial
 from utils.constants import LIN_REG_PARAMS
+import logging
+
+log = logging.getLogger("linear_and_logistic")
 
 
-def linear_regression(data: Callable) -> pm.Model:
+def linear_regression(data: Callable, samples=None) -> pm.Model:
 
     x, y = data()
     basic_model = pm.Model()
@@ -17,38 +20,29 @@ def linear_regression(data: Callable) -> pm.Model:
         sigma = HalfCauchy("sigma", beta=10, testval=1.0)
         intercept = Normal("Intercept", 0, sigma=20)
         x_coeff = Normal("x", 0, sigma=20)
-
         likelihood = Normal("y", mu=intercept + x_coeff * x, sigma=sigma, observed=y)
-
-    return basic_model
+        map = map_estimation(basic_model, method="powell")
+        if samples is not None:
+            if not isinstance(samples, int):
+                raise(ValueError, "samples arg must be int")
+            elif samples < 50:
+                raise(ValueError, "samples, must be greater than 50")
+            else:
+                trace = pm.sample(samples)
+                traceplot(trace)
+    return basic_model, trace
 
 
 def logistic_regression():
     pass
 
 
-def traceplot(trace):
-    plt.figure(figsize=(7, 7))
-    traceplot(trace[100:])
-    plt.tight_layout()
-
-
-def plot_posterior_predictive_samples(data: Callable, samples=100):
-
-    plt.figure(figsize=(7, 7))
-    plt.plot(x, y, "x", label="data")
-    plot_posterior_predictive_glm(
-        trace, samples=samples, label="posterior predictive regression lines"
-    )
-
-    plt.title("Posterior predictive regression lines")
-    plt.legend(loc=0)
-    plt.xlabel("x")
-    plt.ylabel("y")
-
-
 if __name__ == "__main__":
     data_gen = partial(simulate_data_lr, **LIN_REG_PARAMS)
     basic_model: pm.Model = linear_regression(data_gen)
-    map = map_estimation(basic_model, method="powell")
+    log.info("Running MAP mode")
+
     print(map)
+    log.info("Running NUTS")
+    basic_model: pm.Model = linear_regression(data_gen, samples=500)
+    ppc = pm.sample_posterior_predictive(trace, samples=500, model=model)
