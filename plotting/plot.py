9cdd3d87cda2f14dc764493506794d5f9e5e848e
Ryan Nazareth <ryankarlos@gmail.com>
2021-09-26 19:56:58 +0100

    transfer plots and notebook to general net repo

--- plotting/plot.py
+++ plotting/plot.py
@@ -20,38 +20,6 @@ def draw_network(G, ax, edge_list=None, color="red"):
         return G
 
 
-def plot_community_class_count(communities):
-    count_list = []
-    class_list = []
-    for i, c in enumerate(communities):
-        class_list.append(i)
-        count_list.append(len(list(c)))
-
-    df = pd.DataFrame({"class": class_list, "count": count_list})
-    df.plot.bar(x="class", y="count")
-    return df
-
-
-def plot_subgraph(G, node_list, ax):
-    subgraph = G.subgraph(node_list)
-    diameter = nx.diameter(subgraph)
-    print("Network diameter of largest component:", diameter)
-    draw_network(subgraph, color="red", ax=ax)
-    return subgraph
-
-
-def plot_ego_network(G, n, radius, ax):
-    """
-    plot ego network around a node n depending
-    on radius setting i.e. only include upto
-    n nodes directly or indirectly connected to
-    this node
-    """
-    ego_nx = ego_graph(G, n, radius=radius)
-    draw_network(ego_nx, ax=ax, color="red")
-    return ego_nx
-
-
 def plot_interactive_network(model, title):
     net = Network(notebook=True)
     net.from_nx(model)
